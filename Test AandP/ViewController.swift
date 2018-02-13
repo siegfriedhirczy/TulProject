718a51969f92e18fe58a29f51d3ff01d5bc6983c
Siegfried Hirczy <Siegfried@miras-air-2.fios-router.home>
2018-02-13 15:26:50 -0500

    Initial Commit

---
+++ Test AandP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test AandP
+//
+//  Created by Siegfried Hirczy on 2/13/18.
+//  Copyright © 2018 Siegfried Hirczy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Test AandPUITests/Test_AandPUITests.swift
@@ -0,0 +1,36 @@
+//
+//  Test_AandPUITests.swift
+//  Test AandPUITests
+//
+//  Created by Siegfried Hirczy on 2/13/18.
+//  Copyright © 2018 Siegfried Hirczy. All rights reserved.
+//
+
+import XCTest
+
+class Test_AandPUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
